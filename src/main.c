c0975afdc54f721dcda5c3cf4f3f1757a8a4c149
h3nnn4n <uber.renan@gmail.com>
2020-05-27 08:44:08 -0300

    Colorful triangle

--- src/main.c
+++ src/main.c
@@ -3,18 +3,6 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-float vertices[] = {
-     0.5f,  0.5f, 0.0f,  // top right
-     0.5f, -0.5f, 0.0f,  // bottom right
-    -0.5f, -0.5f, 0.0f,  // bottom left
-    -0.5f,  0.5f, 0.0f   // top left
-};
-
-unsigned int indices[] = {  // note that we start from 0!
-    0, 1, 3,   // first triangle
-    1, 2, 3    // second triangle
-};
-
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
   glViewport(0, 0, width, height);
@@ -26,21 +14,23 @@ void processInput(GLFWwindow *window)
     glfwSetWindowShouldClose(window, 1);
 }
 
-const char *vertexShaderSource =
-  "#version 330 core\n"
+const char *vertexShaderSource ="#version 330 core\n"
   "layout (location = 0) in vec3 aPos;\n"
+  "layout (location = 1) in vec3 aColor;\n"
+  "out vec3 ourColor;\n"
   "void main()\n"
   "{\n"
-  "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+  "   gl_Position = vec4(aPos, 1.0);\n"
+  "   ourColor = aColor;\n"
   "}\0";
 
-const char *fragmentShaderSource =
-  "#version 330 core\n"
+const char *fragmentShaderSource = "#version 330 core\n"
   "out vec4 FragColor;\n"
+  "in vec3 ourColor;\n"
   "void main()\n"
   "{\n"
-  "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-  "}\n";
+  "   FragColor = vec4(ourColor, 1.0f);\n"
+  "}\n\0";
 
 int main()
 {
@@ -115,24 +105,27 @@ int main()
   glDeleteShader(vertexShader);
   glDeleteShader(fragmentShader);
 
-  unsigned int VBO;
-  glGenBuffers(1, &VBO);
+  float vertices[] = {
+    // positions         // colors
+     0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
+    -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
+     0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top
+  };
 
-  unsigned int VAO;
+  unsigned int VBO, VAO;
+
   glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &VBO);
   glBindVertexArray(VAO);
-
-  unsigned int EBO;
-  glGenBuffers(1, &EBO);
 
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
   glEnableVertexAttribArray(0);
+
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+  glEnableVertexAttribArray(1);
 
   /*glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);*/
   /*glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);*/
@@ -144,8 +137,10 @@ int main()
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
+    glUseProgram(shaderProgram);
+
     glBindVertexArray(VAO);
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
     glBindVertexArray(0);
 
     glfwSwapBuffers(window);
