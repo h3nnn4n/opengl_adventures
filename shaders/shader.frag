b01dad9a435fa8e104d9b7e509ec18cd37d77ce7
h3nnn4n <uber.renan@gmail.com>
2020-05-27 21:27:38 -0300

    Overlay 2 textures

--- shaders/shader.frag
+++ shaders/shader.frag
@@ -4,7 +4,9 @@ out vec4 FragColor;
 in vec3 ourColor;
 in vec2 TexCoord;
 
-uniform sampler2D ourTexture;
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+
 uniform float colorOverrideIntensity;
 uniform float time;
 
@@ -19,6 +21,12 @@ void main()
     1.0
   );
 
-  FragColor = vec4(ourColor, 1.0) + override;
-  FragColor+= texture(ourTexture, TexCoord);
+  FragColor = (vec4(ourColor, 1.0) + override) * 0.5;
+  /*FragColor = texture(texture1, TexCoord);*/
+  /*FragColor = texture(texture2, TexCoord);*/
+  FragColor += mix(
+    texture(texture1, TexCoord),
+    texture(texture2, TexCoord),
+    0.2
+  );
 }
--- src/main.c
+++ src/main.c
@@ -104,35 +104,63 @@ int main()
 
   glBindVertexArray(0);
 
-  /*glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);*/
-  /*glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);*/
+  unsigned int texture1, texture2;
+
+  glGenTextures(1, &texture1);
+  glBindTexture(GL_TEXTURE_2D, texture1);
 
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
 
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  stbi_set_flip_vertically_on_load(1);
 
-  int width, height, nrChannels;
-  const char *tex_filename = "assets/container.jpg";
-  unsigned char *data = stbi_load(tex_filename, &width, &height, &nrChannels, 0);
+  {
+    int width, height, nrChannels;
+    const char *tex_filename = "assets/container.jpg";
+    unsigned char *data = stbi_load(tex_filename, &width, &height, &nrChannels, 0);
 
-  unsigned int texture;
-  glGenTextures(1, &texture);
+    if (data) {
+      glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+      glGenerateMipmap(GL_TEXTURE_2D);
+    } else {
+      fprintf(stderr, "Failed to load %s", tex_filename);
+    }
 
-  glBindTexture(GL_TEXTURE_2D, texture);
-
-  if (data) {
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-    glGenerateMipmap(GL_TEXTURE_2D);
-  } else {
-    fprintf(stderr, "Failed to load %s", tex_filename);
+    stbi_image_free(data);
   }
 
-  stbi_image_free(data);
+  glGenTextures(1, &texture2);
+  glBindTexture(GL_TEXTURE_2D, texture2);
+
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
+
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+  {
+    int width, height, nrChannels;
+    const char *tex_filename = "assets/awesomeface.png";
+    unsigned char *data = stbi_load(tex_filename, &width, &height, &nrChannels, 0);
+
+    if (data) {
+      glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+      glGenerateMipmap(GL_TEXTURE_2D);
+    } else {
+      fprintf(stderr, "Failed to load %s", tex_filename);
+    }
+
+    stbi_image_free(data);
+  }
+
+  Shader_set_int(shader, "texture1", 0);
+  Shader_set_int(shader, "texture2", 1);
+
+  /*glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);*/
+  /*glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);*/
 
   while(!glfwWindowShouldClose(window))
   {
@@ -151,8 +179,16 @@ int main()
     Shader_set_float(shader, "y_diff", y_diff);
 
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBindTexture(GL_TEXTURE_2D, texture);
+
+    /*glBindTexture(GL_TEXTURE_2D, texture);*/
+
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture1);
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+
     glBindVertexArray(VAO);
+
     /*glDrawArrays(GL_TRIANGLES, 0, 3);*/
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
     glBindVertexArray(0);
