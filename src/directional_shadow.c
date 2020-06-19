e38de5ae9f8c3ae670c116eb2e73d84595112aea
h3nnn4n <uber.renan@gmail.com>
2020-06-19 19:32:21 -0300

    Tweak shadow map

--- src/directional_shadow.c
+++ src/directional_shadow.c
@@ -29,8 +29,11 @@ void build_directional_shadow_map() {
 
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+
+  float borderColor[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+  glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor);
 
   glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
   glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depthMap, 0);
@@ -53,9 +56,9 @@ void render_directional_shadow_map() {
   glClear(GL_DEPTH_BUFFER_BIT);
 
   {
-    float light_distance = 25.0f;
+    float light_distance = 10.0f;
     float near_plane = 1.0f;
-    float far_plane = 100.0f;
+    float far_plane = 25.0f;
 
     glm_ortho(-10.0f, 10.0f, -10.0f, 10.0f, near_plane, far_plane, directional_light->shadow_projection);
 
